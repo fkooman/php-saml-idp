c467ceefc783ad0f7005b23eae38929c2cf13cb3
François Kooman <fkooman@tuxed.net>
2018-12-19 16:08:16 +0100

    implement logout

---
+++ views/logout.php
@@ -0,0 +1,7 @@
+<?php $this->layout('base'); ?>
+<?php $this->start('content'); ?>
+    <h2>Logout</h2>
+    <p>
+        You are now logged out.
+    </p>
+<?php $this->stop(); ?>
---
+++ web/logout.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * Copyright (c) 2018 François Kooman <fkooman@tuxed.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+require_once \dirname(__DIR__).'/vendor/autoload.php';
+
+use fkooman\SAML\IdP\ErrorHandler;
+use fkooman\SAML\IdP\Template;
+use fkooman\SeCookie\Cookie;
+use fkooman\SeCookie\Session;
+
+ErrorHandler::register();
+
+$baseDir = \dirname(__DIR__);
+
+$tpl = new Template([\sprintf('%s/views', $baseDir)]);
+
+try {
+    $session = new Session(
+        [],
+        new Cookie(
+            [
+               'SameSite' => 'Lax',
+            ]
+        )
+    );
+    $session->destroy();
+
+    echo $tpl->render('logout');
+} catch (Exception $e) {
+    echo $tpl->render('error', ['errorCode' => 500, 'errorMessage' => $e->getMessage()]);
+}
