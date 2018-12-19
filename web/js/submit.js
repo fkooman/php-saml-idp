03e89edfb662b13ee646c5724cdd69168e506d3d
François Kooman <fkooman@tuxed.net>
2018-12-19 15:15:05 +0100

    remove submit.js

--- web/js/submit.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", function () {
-    // automatically submit the form so the SP will receive the SAMLResponse
-    // on its ACS without user involvement...
-    document.forms["submit"].submit();
-});
