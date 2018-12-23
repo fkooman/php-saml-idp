b50aa86d420f9e8c3df19c3fe1873bbc4cfc3678
François Kooman <fkooman@tuxed.net>
2018-12-23 14:47:31 +0100

    Revert "sign LogoutResponse"
    This reverts commit c62c502fb9d5f8c1aa4fcd8e0f34f851df4515f2.

--- views/logoutResponse.php
+++ views/logoutResponse.php
@@ -1,25 +1,5 @@
 <samlp:LogoutResponse xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="<?=$this->e($id); ?>" Version="2.0" IssueInstant="<?=$this->e($issueInstant); ?>" Destination="<?=$this->e($destination); ?>" InResponseTo="<?=$this->e($inResponseTo); ?>">
     <saml:Issuer><?=$this->e($issuer); ?></saml:Issuer>
-    <ds:Signature xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
-        <ds:SignedInfo>
-            <ds:CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
-            <ds:SignatureMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"/>
-             <ds:Reference URI="#<?=$this->e($id); ?>">
-                <ds:Transforms>
-                    <ds:Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/>
-                    <ds:Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
-                </ds:Transforms>
-                <ds:DigestMethod Algorithm="http://www.w3.org/2001/04/xmlenc#sha256"/>
-                <ds:DigestValue />
-            </ds:Reference>
-        </ds:SignedInfo>
-        <ds:SignatureValue />
-        <ds:KeyInfo>
-            <ds:X509Data>
-                <ds:X509Certificate><?=$this->e($x509Certificate); ?></ds:X509Certificate>
-            </ds:X509Data>
-        </ds:KeyInfo>
-    </ds:Signature>
     <samlp:Status>
         <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Success"/>
     </samlp:Status>
