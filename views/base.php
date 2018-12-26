4e6753831494c21f7d12fdb0c24065a9c791933c
François Kooman <fkooman@tuxed.net>
2018-12-26 13:44:05 +0100

    text changes in base template

--- views/base.php
+++ views/base.php
@@ -5,19 +5,19 @@
     <meta charset="utf-8">
     <meta name="application-name" content="php-saml-idp">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$this->t('SAML Identity Provider'); ?></title>
+    <title><?=$this->t('Identity Provider'); ?></title>
     <link rel="stylesheet" type="text/css" href="css/milligram.min.css">
     <link rel="stylesheet" type="text/css" href="css/screen.css">
     <?=$this->section('js'); ?>
 </head>
 <body>
-    <h1><?=$this->t('SAML Identity Provider'); ?></h1>
+    <h1><?=$this->t('Identity Provider'); ?></h1>
     <div class="content">
         <?=$this->section('content'); ?>
     </div> <!-- container -->
 
     <div class="footer">
-        <a href="https://git.tuxed.net/fkooman/php-saml-idp"><?=$this->t('SAML Identity Provider'); ?></a>
+        <a href="https://git.tuxed.net/fkooman/php-saml-idp"><?=$this->t('php-saml-idp'); ?></a>
     </div> <!-- footer -->
 </body>
 </html>
