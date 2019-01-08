83736739398641340ab81a0ae2c7341b2a6f4e72
François Kooman <fkooman@tuxed.net>
2019-01-08 19:30:17 +0100

    update CSS, add normalize

--- views/base.php
+++ views/base.php
@@ -6,7 +6,8 @@
     <meta name="application-name" content="php-saml-idp">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$this->t('Identity Provider'); ?></title>
-    <link rel="stylesheet" type="text/css" href="css/milligram.min.css">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/milligram.css">
     <link rel="stylesheet" type="text/css" href="css/screen.css">
 </head>
 <body>
