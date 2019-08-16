82201e01ac022b31909e80b682052dab680769e7
François Kooman <fkooman@tuxed.net>
2019-08-16 19:08:30 +0200

    small viewport fix

--- src/tpl/base.php
+++ src/tpl/base.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="application-name" content="php-saml-idp">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$this->t('Identity Provider'); ?></title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
     <link rel="stylesheet" type="text/css" href="css/screen.css">
